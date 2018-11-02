40c9f79a56681f6c181124696854b54e6161de12
Nga92 <nguyenthinga.260392@gmail.com>
2018-11-02 16:54:46 +0700

    fix

---
+++ MAVEN_BANGGURU_05_NGANT/src/main/java/commons/AbstractTest.java
@@ -0,0 +1,39 @@
+package commons;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AbstractTest {
+	WebDriver driver;
+
+	public WebDriver openMutilBrowser(String browserName,String url) {
+		if (browserName.equals("chrome")) {
+			 System.setProperty("webdriver.chrome.driver",".\\src\\test\\resources\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equals("firefox")) {
+			driver = new FirefoxDriver();
+		} else {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("headless");
+			options.addArguments("window-size=1920x1080");
+			driver = new ChromeDriver(options);
+		}
+		 driver.get(url);
+		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		return driver;
+	}
+
+	   public int random() {
+		   Random rand = new Random();
+		   int  number = rand.nextInt(999999) + 1;
+		   return number;
+	   }
+	public void quitBrowser() {
+		driver.quit();
+	}
+}
